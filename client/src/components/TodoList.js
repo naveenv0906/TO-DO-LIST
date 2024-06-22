e62c48f62c5cb79a99ee6a6e8a9a3c67a081c54f
naveenv0906 <naveenv0906@gmail.com>
2024-06-22 17:22:45 +0530

    first commit

---
+++ client/src/components/TodoList.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+    const [task, setTask] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/todos')
+            .then(response => setTodos(response.data))
+            .catch(error => console.error('Error fetching todos:', error));
+    }, []);
+
+    const addTodo = () => {
+        console.log('Adding task:', task); // Log task to be added
+        axios.post('http://localhost:5000/api/todos', { task })
+            .then(response => {
+                console.log('Task added:', response.data); // Log success response
+                setTodos([...todos, response.data]);
+            })
+            .catch(error => console.error('Error adding todo:', error)); // Log error
+        setTask('');
+    };
+
+    const deleteTodo = (id) => {
+        axios.delete(`http://localhost:5000/api/todos/${id}`)
+            .then(() => setTodos(todos.filter(todo => todo.id !== id)))
+            .catch(error => console.error('Error deleting todo:', error));
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center">To-Do List</h1>
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="New task"
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <button className="btn btn-primary" onClick={addTodo}>Add</button>
+            </div>
+            <ul className="list-group">
+                {todos.map(todo => (
+                    <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
+                        {todo.task}
+                        <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASS,
+    database: 'todo_list'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+app.get('/api/todos', (req, res) => {
+    db.query('SELECT * FROM todos', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.post('/api/todos', (req, res) => {
+    const { task } = req.body;
+    console.log('Received task:', task);
+    db.query('INSERT INTO todos (task) VALUES (?)', [task], (err, results) => {
+        if (err) {
+            console.error('Error inserting task:', err); 
+            res.status(500).send(err);
+            return;
+        }
+        console.log('Task inserted with ID:', results.insertId); 
+        res.status(201).send({ id: results.insertId, task });
+    });
+});
+
+app.delete('/api/todos/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('DELETE FROM todos WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.status(200).send({ id });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
